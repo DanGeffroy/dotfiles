b346ee78bbc5fa3d72811991aed02eba0c37163e
DanGeffroy <ogzmovie@gmail.com>
2022-08-09 22:20:50 +0200

    feat: adding correct hyperline config

--- .hyper.js
+++ .hyper.js
@@ -130,6 +130,10 @@ module.exports = {
         // set to true to preserve working directory when creating splits or tabs
         preserveCWD: true,
         // for advanced config flags please refer to https://hyper.is/#cfg
+
+        hyperline: {                                                                 
+            plugins: ['hostname', 'ip', 'memory', 'battery','cpu', 'network']                                                                          
+        }
     },
     // a list of plugins to fetch and install from npm
     // format: [@org/]project[#version]
